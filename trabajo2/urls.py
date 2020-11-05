53c46db38c87a3bc24e2bba0bd14c5e9a6b4072c
fsantand <federicosantander95@gmail.com>
2020-11-05 11:53:09 -0300

    Agregado login

--- trabajo2/urls.py
+++ trabajo2/urls.py
@@ -21,4 +21,5 @@ from django.conf  import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('interlancer.urls')),
+    path('usuario/', include('usuario.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- usuario/views.py
+++ usuario/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib import auth, messages
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username = username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'Estas conectado ahora !')
+            return redirect('home')
+        else:
+            messages.error(request, 'Credenciales inexistentes o erroneas')
+            return redirect('login')
+    if request.method == 'GET':
+        return render(request, 'usuario/login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
