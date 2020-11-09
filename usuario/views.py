a64f26f6b27c9e4aafa64d36aacb27c1f5708108
fsantand <federicosantander95@gmail.com>
2020-11-08 21:12:18 -0300

    Implementado registro de usuarios

--- usuario/views.py
+++ usuario/views.py
@@ -1,6 +1,10 @@
 from django.contrib import auth, messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+from .models import Interprete, Solicitante
+from .forms import RegisterForm
 
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm
