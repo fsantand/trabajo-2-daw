014ea25a9191ee9a99633edae6c9bd4374f4f223
dQuezada-P <diegoquezadapavez@gmail.com>
2020-11-08 23:00:33 -0300

    Atencion
    Busqueda de interpretes por especialidad y muestreo de solicitud de atencion

--- atencion/views.py
+++ atencion/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
-
+from django.contrib.auth.decorators import login_required
 from .forms import SolicitudForm
 from .models import Atencion
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class SolicitudView(CreateView):
+
+class SolicitudView(LoginRequiredMixin,CreateView):
     model = Atencion
     template_name = 'atencion/solicitar.html'
     form_class = SolicitudForm
@@ -13,3 +15,4 @@ class SolicitudView(CreateView):
     def form_valid(self, form):
         form.save(self.request.user)
         return super(SolicitudView, self).form_valid(form)
+
