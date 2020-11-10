59af6cdca0bb033463dc9235150c148fb490e343
fsantand <federicosantander95@gmail.com>
2020-11-09 21:36:17 -0300

    Agregada pagina de error 404

--- trabajo2/urls.py
+++ trabajo2/urls.py
@@ -18,9 +18,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf  import settings
 
+handler404 = 'interlancer.views.page_not_found'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('interlancer.urls')),
     path('usuario/', include('usuario.urls')),
     path('atencion/', include('atencion.urls')),
+    path('review/', include('review.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
