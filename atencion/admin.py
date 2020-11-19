247be095ced2cb311d0eaf8d8c2986fc514d7cac
fsantand <federicosantander95@gmail.com>
2020-11-19 01:10:25 -0300

    Implementadas sesion instantaneas

--- atencion/admin.py
+++ atencion/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Atencion
+from .models import Atencion, Sesion
 
 # Register your models here.
 admin.site.register(Atencion)
+admin.site.register(Sesion)
