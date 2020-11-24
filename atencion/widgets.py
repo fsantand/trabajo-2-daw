62a0a5d1b596ee7853b841aa5a389f8afd04ec01
dQuezada-P <diegoquezadapavez@gmail.com>
2020-11-23 21:53:41 -0300

    se agrega formato de fecha a formulario de solicitar atencion

--- atencion/widgets.py
+++ atencion/widgets.py
@@ -1,7 +1,20 @@
 from django.forms import DateTimeInput
 
-class BootstrapDateTimePickerInput(DateTimeInput):
-    template_name = 'widgets/bootstrap_datetimepicker.html'
+class BootstrapDateTimePickerInputCreate(DateTimeInput):
+    template_name = 'widgets/bootstrap_datetimepicker_create.html'
+
+    def get_context(self, name, value, attrs):
+        datetimepicker_id = 'datetimepicker_{name}'.format(name=name)
+        if attrs is None:
+            attrs = dict()
+        attrs['data-target'] = '#{id}'.format(id=datetimepicker_id)
+        attrs['class'] = 'form-control datetimepicker-input'
+        context = super().get_context(name, value, attrs)
+        context['widget']['datetimepicker_id'] = datetimepicker_id
+        return context
+
+class BootstrapDateTimePickerInputModify(DateTimeInput):
+    template_name = 'widgets/bootstrap_datetimepicker_modify.html'
 
     def get_context(self, name, value, attrs):
         datetimepicker_id = 'datetimepicker_{name}'.format(name=name)
